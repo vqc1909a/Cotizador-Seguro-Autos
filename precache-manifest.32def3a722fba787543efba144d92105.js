401c79c1ee9f9be92015fb64d2d7fc14fb033469
Nero Angel <neroangel1909@gmail.com>
2021-05-09 21:35:49 -0500

    Updates

---
+++ precache-manifest.32def3a722fba787543efba144d92105.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e785f6270a2e98faf609f8d36a8d6b6",
+    "url": "/Cotizador-Seguro-Autos/index.html"
+  },
+  {
+    "revision": "6844091eda4588167ab4",
+    "url": "/Cotizador-Seguro-Autos/static/css/main.d32facc7.chunk.css"
+  },
+  {
+    "revision": "40130c804e3bdb7f5f5f",
+    "url": "/Cotizador-Seguro-Autos/static/js/2.146d96b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cotizador-Seguro-Autos/static/js/2.146d96b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6844091eda4588167ab4",
+    "url": "/Cotizador-Seguro-Autos/static/js/main.ce6fec2d.chunk.js"
+  },
+  {
+    "revision": "67be1cc2523f4280d1c65f8ac8c36978",
+    "url": "/Cotizador-Seguro-Autos/static/js/main.ce6fec2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5c8c16235132b5bb0ce",
+    "url": "/Cotizador-Seguro-Autos/static/js/runtime-main.ee21882d.js"
+  }
+]);
